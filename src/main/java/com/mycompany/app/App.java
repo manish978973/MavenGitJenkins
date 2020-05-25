3f80c806dc3f70376e2772d96bd2175ba2dd801a
manish978973 <manishrchandran@gmail.com>
2020-05-25 22:12:31 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World..this has been changed!";
+    private final String message = "Hello World!";
 
     public App() {}
 
